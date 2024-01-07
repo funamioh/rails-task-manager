590374b1f7bd6648a1125d40ff101c1a70f535cc
Miho Funayama <funamiho1011@gmail.com>
2024-01-07 22:53:26 +0900

    first commit after modifying

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "tasks", to: "tasks#index", as: :tasks
+  # go to create a task page
+  get "tasks/new", to: "tasks#new", as: :new_task
+
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  # post a new task
+  post "tasks", to: "tasks#create"
 end
